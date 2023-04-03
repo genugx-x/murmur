95dc5aa7a37913d35b363f80db63ddce74938ffa
genugx-x <qparanoya@gmail.com>
2023-04-03 14:50:05 +0900

    #2 - 간단한 ErrorHandler 코드 작성 및 테스트

--- src/main/java/com/genug/murmur/api/controller/PostController.java
+++ src/main/java/com/genug/murmur/api/controller/PostController.java
@@ -3,12 +3,10 @@ package com.genug.murmur.api.controller;
 import com.genug.murmur.api.request.PostCreate;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -20,19 +18,7 @@ public class PostController {
 
     // 글 등록
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate postCreate, BindingResult result) {
-        log.info("{}", postCreate);
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError first = fieldErrors.get(0);
-            String fieldName = first.getField();
-            String errorMessage = first.getDefaultMessage();
-
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
-
+    public Map<String, String> post(@RequestBody @Valid PostCreate postCreate) {
         return Map.of();
     }
 
