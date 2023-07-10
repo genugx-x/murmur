1916d970719e88e132772636df59e5feaa4d3cb7
genugx-x <qparanoya@gmail.com>
2023-07-10 14:30:13 +0900

    api#3 - Builder 적용 및 관련 코드 수정

--- src/main/java/com/genug/murmur/api/controller/PostController.java
+++ src/main/java/com/genug/murmur/api/controller/PostController.java
@@ -1,25 +1,30 @@
 package com.genug.murmur.api.controller;
 
 import com.genug.murmur.api.request.PostCreate;
+import com.genug.murmur.api.service.PostService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     // Http Method
     // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
 
     // 글 등록
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate postCreate) {
-        return Map.of();
+    public ResponseEntity<?> post(@RequestBody @Valid PostCreate request) {
+        Long postId = postService.write(request);
+        return ResponseEntity.ok(postId);
     }
 
 
--- src/test/java/com/genug/murmur/api/controller/PostControllerTest.java
+++ src/test/java/com/genug/murmur/api/controller/PostControllerTest.java
@@ -1,33 +1,58 @@
 package com.genug.murmur.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.genug.murmur.api.repository.PostRepository;
+import com.genug.murmur.api.request.PostCreate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
 
-    @Autowired // @WebMvcTest 를 테스트클래스에 추가해줘야함
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired // @AutoConfigureMockMvc를 테스트클래스에 추가해줘야함
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach // 각각의 테스트 메서드가 수행되기 전에 수행됨.
+    void clean() {
+        postRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
     void test() throws Exception {
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
+        // System.out.println(json);
+
         // expected - MockMvcRequestBuilders.get() .post()
         mockMvc.perform(post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"궁시렁궁시렁...\", \"content\":\"어쩌구저쩌구\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpectAll(
                         status().isOk(),
-                        content().string("{}")
+                        content().string("1")
                 )
                 .andDo(print());
     }
@@ -35,10 +60,13 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 title 입력은 필수다. ")
     void test2() throws Exception {
-        // expected - MockMvcRequestBuilders.get() .post()
+        PostCreate request = PostCreate.builder()
+                .content("내용입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
         mockMvc.perform(post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"\", \"content\":\"어쩌구저쩌구\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpectAll(
                         status().isBadRequest(),
@@ -52,10 +80,13 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청시 content 입력은 필수다. ")
     void test3() throws Exception {
-        // expected - MockMvcRequestBuilders.get() .post()
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
         mockMvc.perform(post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\": \"궁시렁궁시렁...\", \"content\":\"\"}")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
                 )
                 .andExpectAll(
                         status().isBadRequest(),
@@ -63,6 +94,27 @@ class PostControllerTest {
                         jsonPath("$.validation.content").value("내용을 입력해주세요.")
                 )
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/posts 요청시 DB에 값이 저장된다.")
+    void test4() throws Exception {
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
+        mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpectAll(
+                        status().isOk(),
+                        content().string("1")
+                )
+                .andDo(print());
+        // then
+        assertEquals(1L, postRepository.count());
     }
 
 }
